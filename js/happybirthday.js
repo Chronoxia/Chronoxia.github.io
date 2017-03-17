ea6c1000779109f165981b7cf74f493c77e7a13e
Chronoxia <forwearemany@yandex.ru>
2017-03-17 07:46:12 +0300

    Update happybirthday.js

--- js/happybirthday.js
+++ js/happybirthday.js
@@ -4,7 +4,7 @@
 
 		var answer = prompt('If you have not received password yet, then press cancel.', '');
 		if (answer == 'Kirill') {
-			alert("'u.' I hope you are enjoying it, because I feel kinda stupid :(");
+			alert("'u.' I hope you enjoyed it, because I feel kinda stupid :( Last one is your favorite number.");
 		} else if (answer == null) {
 			alert('Just smash F5 button to try again later.');
 		} else {
