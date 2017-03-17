00acdf6039fd10b073677557b59c750936472096
Chronoxia <forwearemany@yandex.ru>
2017-03-17 05:44:44 +0300

    Create happybirthday.js

---
+++ js/happybirthday.js
@@ -0,0 +1,17 @@
+
+
+		var user = prompt("Who's there?", "");
+		if (user == 'arinoris' || 'Arinoris' || 'ari' || 'Ari' || 'Rita' || 'Margarita' || 'rita' || 'margarita') {
+			alert('Hi cutie');
+		}		
+		else { 
+			alert('Hi stranger');
+		}
+
+		var answer = prompt("If you didn't receive password, press cancel.", "");
+		if (answer = 'Kirill' || 'kirill') {
+			alert("I hope you're enjoying it, because I feel kinda stupid :( 'r' ");
+		}
+		else {
+			alert('Just smash F5 button to try again');
+		}
