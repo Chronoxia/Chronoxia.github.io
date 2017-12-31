af1c9dbdd6e963f2577f9666686dd1a38b6bd061
Chronoxia <forwearemany@yandex.ru>
2017-12-31 18:20:26 +0300

    Update happybirthday.js

--- js/happybirthday.js
+++ js/happybirthday.js
@@ -2,11 +2,11 @@
 
 		'use strict'
 
-		var answer = prompt('If you have not received password yet, then press cancel.', '');
-		if (answer == 'Kirill') {
-			alert("#9 is 'u.' I hope you enjoyed it, because I feel kinda stupid :( Last one is your favorite number.");
-		} else if (answer == null) {
-			alert('Just smash F5 button to try again later.');
-		} else {
-			alert('Wrong. Just smash F5 button to try again later.');
-		}
+// 		var answer = prompt('If you have not received password yet, then press cancel.', '');
+// 		if (answer == 'Kirill') {
+// 			alert("#9 is 'u.' I hope you enjoyed it, because I feel kinda stupid :( Last one is your favorite number.");
+// 		} else if (answer == null) {
+// 			alert('Just smash F5 button to try again later.');
+// 		} else {
+// 			alert('Wrong. Just smash F5 button to try again later.');
+// 		}
